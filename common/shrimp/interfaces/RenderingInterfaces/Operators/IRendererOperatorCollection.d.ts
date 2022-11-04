2bde20133becb8b0b564070b77699eec7ee94d4f
omerbozabaSofttech <omerbozaba@softtech.com.tr>
2022-11-04 11:20:47 +0300

    library app

--- common/shrimp/interfaces/RenderingInterfaces/Operators/IRendererOperatorCollection.d.ts
+++
@@ -1,7 +0,0 @@
-import { IComponentOperator } from "./IComponentOperator";
-import { IEventOperator } from "./IEventOperator";
-export interface IRendererOperatorCollection {
-    eventOperator?: IEventOperator;
-    componentOperator?: IComponentOperator;
-}
-//# sourceMappingURL=IRendererOperatorCollection.d.ts.map
