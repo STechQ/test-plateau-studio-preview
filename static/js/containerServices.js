b4537d1654a2cd1598dc9b4236b40165a39aec5d
mesime ezgi gökçay alagöz <65234948+megokcay@users.noreply.github.com>
2023-02-03 13:10:42 +0300

    Add files via upload
    containerServices File

---
+++ static/js/containerServices.js
@@ -0,0 +1,13 @@
+
+var containerServices = {
+    extensions: {    
+    }
+}
+
+
+if (typeof exports === 'object' && typeof module === 'object') {
+    module.exports = containerServices;
+  } else {
+    window["plateauUIContainerServices"] = containerServices;
+  }
+//#region JS Section
